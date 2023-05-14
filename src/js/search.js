d9ce125ee57f215e0943e352e9d7ef46e23817da
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-14 12:07:05 +0300

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -12,16 +12,16 @@ const refs = {
   charactersGallery: document.querySelector('.characters-gallery'),
 };
 // console.log(refs.dateInput);
-// const marvelApi = new MarvelAPI();
+const marvelApi = new MarvelAPI();
 
-const PUBLIC_KEY = 'e8d87ed088b5013742a2a9466816b30e';
-const PRIVATE_KEY = 'dbde977f898ea7131460b979ad9d4adf2e774ce4';
+// const PUBLIC_KEY = 'e8d87ed088b5013742a2a9466816b30e';
+// const PRIVATE_KEY = 'dbde977f898ea7131460b979ad9d4adf2e774ce4';
 
 // Create a character card
 function createCharacterCard(character) {
   const card = document.createElement('li');
   card.innerHTML = `
-    <img src="${character.thumbnail.path}/${imageSize}.${
+    <img src="${character.thumbnail.path}.${
     character.thumbnail.extension
   }" alt="${character.name}">
     <h3>${character.name}</h3>
@@ -32,40 +32,40 @@ function createCharacterCard(character) {
 
 // Render character cards in the page
 function renderCharacterCards(characters) {
-  const ul = document.querySelector('ul');
-  ul.innerHTML = defaultImage;
+  // const ul = document.querySelector('ul');
+  // refs.charactersGallery.innerHTML = defaultImage;
 
   characters.forEach(character => {
     const card = createCharacterCard(character);
-    ul.appendChild(card);
+    refs.charactersGallery.appendChild(card);
   });
 }
 
 // Fetch characters
-async function fetchCharacters(comics, orderBy, nameStartsWith, modifiedSince) {
-  const baseUrl = 'https://gateway.marvel.com/v1/public/characters';
-  const timestamp = new Date().getTime();
-  const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
-  const params = new URLSearchParams({
-    apikey: PUBLIC_KEY,
-    comics: comics || undefined,
-    orderBy: orderBy || undefined,
-    nameStartsWith: nameStartsWith || undefined,
-    modifiedSince: modifiedSince || undefined,
-    ts: timestamp,
-    hash: hash,
-  });
+// async function fetchCharacters(comics, orderBy, nameStartsWith, modifiedSince) {
+//   const baseUrl = 'https://gateway.marvel.com/v1/public/characters';
+//   const timestamp = new Date().getTime();
+//   const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
+//   const params = new URLSearchParams({
+//     apikey: PUBLIC_KEY,
+//     // comics: comics || null,
+//     // orderBy: orderBy || null,
+//     nameStartsWith: nameStartsWith || null,
+//     // modifiedSince: modifiedSince || null,
+//     ts: timestamp,
+//     hash: hash,
+//   });
 
-  const response = await fetch(`${baseUrl}?${params}`);
-  const data = await response.json();
+//   const response = await fetch(`${baseUrl}?${params}`);
+//   const data = await response.json();
 
-  if (data && data.data && data.data.results) {
-    const characters = data.data.results;
-    renderCharacterCards(characters);
-  } else {
-    console.error('Error fetching characters:', data);
-  }
-}
+//   if (data && data.data && data.data.results) {
+//     const characters = data.data.results;
+//     renderCharacterCards(characters);
+//   } else {
+//     console.error('Error fetching characters:', data);
+//   }
+// }
 
 // Event listener
 refs.comicsInput.addEventListener(
@@ -81,7 +81,7 @@ refs.selectInput.addEventListener(
   'change',
   debounce(() => {
     const orderBy = refs.selectInput.value;
-    fetchCharacters(0, orderBy);
+    fetchCharacters(null, orderBy);
   }),
   500
 );
@@ -90,7 +90,10 @@ refs.nameInput.addEventListener(
   'change',
   debounce(() => {
     const nameStartsWith = refs.nameInput.value;
-    fetchCharacters(0, 0, nameStartsWith);
+    // fetchCharacters(null, null, nameStartsWith);
+    // marvelApi.getCharacters().then(data => {
+    //   renderCharacterCards(data);
+    // });
   }),
   500
 );
@@ -99,22 +102,22 @@ refs.dateInput.addEventListener(
   'change',
   debounce(() => {
     const modifiedSince = refs.dateInput.value;
-    fetchCharacters(0, 0, 0, modifiedSince);
+    fetchCharacters(null, null, null, modifiedSince);
   }),
   500
 );
 
-const defaultImage = `<img
-  class="try-looking"
-  srcset="
-                    ./img/tab/frame-tabl-desktop.png 1x,
-                    ./img/tab/frame-2x@tabl-desktop.png 2x
-                  "
-  src="./img/tab/frame-tabl-desktop.png"
-  title="default-image"
-  alt="Try looking for something else"
-  width="375px"
-  height="221px"
-  loading="lazy"
-/>
-`;
+// const defaultImage = `<img
+//   class="try-looking"
+//   srcset="
+//   ./src/img/tab/frame-tabl-desktop.png 1x,
+//   ./src/img/tab/frame-2x@tabl-desktop.png 2x
+//   "
+//   src="./img/tab/frame-tabl-desktop.png"
+//   title="default-image"
+//   alt="Try looking for something else"
+//   width="375px"
+//   height="221px"
+//   loading="lazy"
+// />
+// `;
