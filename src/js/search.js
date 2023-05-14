4c2a2a9e8d41e9654b50522cd0e8dc6a369754eb
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-14 17:25:05 +0300

    js section+api
    API доповнили з Віталіком

--- src/js/search.js
+++ src/js/search.js
@@ -1,123 +1,77 @@
 import MarvelAPI from './api_defaults';
 import debounce from 'lodash.debounce';
-import md5 from 'md5';
-import axios from 'axios';
 
 const refs = {
   body: document.querySelector('body'),
-  comicsInput: document.querySelector("[id = 'comics']"),
-  selectInput: document.querySelector("[id = 'select']"),
-  nameInput: document.querySelector("[id='name-input']"),
-  dateInput: document.querySelector("[id = 'date']"),
+  comicsInput: document.getElementById('comics'),
+  selectInput: document.getElementById('select'),
+  nameInput: document.getElementById('name-input'),
+  dateInput: document.getElementById('date'),
   charactersGallery: document.querySelector('.characters-gallery'),
+  form: document.querySelector('.searchandsort-container'),
 };
-// console.log(refs.dateInput);
-const marvelApi = new MarvelAPI();
 
-// const PUBLIC_KEY = 'e8d87ed088b5013742a2a9466816b30e';
-// const PRIVATE_KEY = 'dbde977f898ea7131460b979ad9d4adf2e774ce4';
+const marvelApi = new MarvelAPI();
 
 // Create a character card
 function createCharacterCard(character) {
   const card = document.createElement('li');
   card.innerHTML = `
-    <img src="${character.thumbnail.path}.${
-    character.thumbnail.extension
-  }" alt="${character.name}">
+    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
     <h3>${character.name}</h3>
-    <p>${character.description || 'No description available'}</p>
   `;
   return card;
 }
 
 // Render character cards in the page
 function renderCharacterCards(characters) {
-  // const ul = document.querySelector('ul');
-  // refs.charactersGallery.innerHTML = defaultImage;
-
+  refs.charactersGallery.innerHTML = '';
   characters.forEach(character => {
     const card = createCharacterCard(character);
     refs.charactersGallery.appendChild(card);
   });
 }
 
-// Fetch characters
-// async function fetchCharacters(comics, orderBy, nameStartsWith, modifiedSince) {
-//   const baseUrl = 'https://gateway.marvel.com/v1/public/characters';
-//   const timestamp = new Date().getTime();
-//   const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
-//   const params = new URLSearchParams({
-//     apikey: PUBLIC_KEY,
-//     // comics: comics || null,
-//     // orderBy: orderBy || null,
-//     nameStartsWith: nameStartsWith || null,
-//     // modifiedSince: modifiedSince || null,
-//     ts: timestamp,
-//     hash: hash,
-//   });
-
-//   const response = await fetch(`${baseUrl}?${params}`);
-//   const data = await response.json();
-
-//   if (data && data.data && data.data.results) {
-//     const characters = data.data.results;
-//     renderCharacterCards(characters);
-//   } else {
-//     console.error('Error fetching characters:', data);
-//   }
-// }
-
 // Event listener
-refs.comicsInput.addEventListener(
+refs.form.addEventListener(
   'change',
-  debounce(() => {
+  debounce(async event => {
     const comics = refs.comicsInput.value;
-    fetchCharacters(comics);
-  }),
-  500
-);
-
-refs.selectInput.addEventListener(
-  'change',
-  debounce(() => {
-    const orderBy = refs.selectInput.value;
-    fetchCharacters(null, orderBy);
-  }),
-  500
-);
-
-refs.nameInput.addEventListener(
-  'change',
-  debounce(() => {
-    const nameStartsWith = refs.nameInput.value;
-    // fetchCharacters(null, null, nameStartsWith);
-    // marvelApi.getCharacters().then(data => {
-    //   renderCharacterCards(data);
-    // });
-  }),
-  500
-);
-
-refs.dateInput.addEventListener(
-  'change',
-  debounce(() => {
+    const selectValue = refs.selectInput.value;
+    const name = refs.nameInput.value;
     const modifiedSince = refs.dateInput.value;
-    fetchCharacters(null, null, null, modifiedSince);
+    console.log(comics, selectValue, name, modifiedSince);
+    const dispayWidth = document.documentElement.clientWidth;
+
+    marvelApi.setPaginationParams(1, 5);
+
+    const data = await marvelApi.getFilteredCharacters(
+      modifiedSince,
+      name,
+      selectValue,
+      comics
+    );
+    if (data.length !== 0) {
+      console.log(marvelApi.totalResults);
+      renderCharacterCards(data);
+    } else {
+      refs.charactersGallery.innerHTML = defaultImage;
+    }
   }),
   500
 );
 
-// const defaultImage = `<img
-//   class="try-looking"
-//   srcset="
-//   ./src/img/tab/frame-tabl-desktop.png 1x,
-//   ./src/img/tab/frame-2x@tabl-desktop.png 2x
-//   "
-//   src="./img/tab/frame-tabl-desktop.png"
-//   title="default-image"
-//   alt="Try looking for something else"
-//   width="375px"
-//   height="221px"
-//   loading="lazy"
-// />
-// `;
+const defaultImage = `<img
+  class="try-looking"
+  srcset="
+  ./img/tab/frame-tabl-deskt.png 1x,
+  ./img/tab/frame-2x@tabl-deskt.png 2x
+  "
+  src="./img/tab/frame-tabl-deskt.png"
+  title="default-image"
+  alt="Try looking for something else"
+  width="375px"
+  height="221px"
+  loading="lazy"
+/>
+`;
