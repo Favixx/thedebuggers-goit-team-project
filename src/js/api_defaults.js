29cb226102ce1145582ec8927c239da5801ccfff
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-05-14 11:04:38 +0300

    random-characters

--- src/js/api_defaults.js
+++ src/js/api_defaults.js
@@ -1,38 +1,40 @@
-import axios from "axios";
-import md5 from "md5";
-export default class MarvelAPI{
-    PUBLIC_KEY = "e8d87ed088b5013742a2a9466816b30e";
-    PRIVATE_KEY = "dbde977f898ea7131460b979ad9d4adf2e774ce4";
-    constructor(){
-        this.marvel = axios.create({
-            baseURL:'https://gateway.marvel.com/v1/public/',
-            params:{
-                apikey: this.PUBLIC_KEY
-            }
-        })
+import axios from 'axios';
+import md5 from 'md5';
+export default class MarvelAPI {
+  PUBLIC_KEY = 'e8d87ed088b5013742a2a9466816b30e';
+  PRIVATE_KEY = 'dbde977f898ea7131460b979ad9d4adf2e774ce4';
+  constructor() {
+    this.marvel = axios.create({
+      baseURL: 'https://gateway.marvel.com/v1/public/',
+      params: {
+        apikey: this.PUBLIC_KEY,
+      },
+    });
+  }
+  async getData(endPoint = 'characters', params = {}) {
+    params.ts = new Date().toDateString();
+    params.hash = md5(params.ts + this.PRIVATE_KEY + this.PUBLIC_KEY);
+    try {
+      const { data, status, statusText } = await this.marvel.get(endPoint, {
+        params,
+      });
+      if (status !== 200) console.log(status, statusText);
+      return data.data.results;
+    } catch (error) {
+      console.log(error.message);
     }
-    async getData(endPoint = 'characters', params = {}){
-        params.ts = new Date().toDateString();
-        params.hash = md5(params.ts+this.PRIVATE_KEY+this.PUBLIC_KEY);
-        try {
-            const { data , status, statusText } = await this.marvel.get(endPoint,{params});
-            if (status !== 200) console.log(status, statusText);
-            return data.data.results;
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
+  }
 
-    async getCharacters(){
-        return await this.getData('characters');
-    }
+  async getCharacters() {
+    return await this.getData('characters');
+  }
 
-    async getCharacterByID(num){
-        return await this.getData(`characters/${num}`);
-    }
+  async getCharacterByID(num) {
+    return await this.getData(`characters/${num}`);
+  }
 
-    setPaginationParams(page = 1, perPage = 20){
-        this.marvel.defaults.params['offset'] = perPage * (page - 1);
-        this.marvel.defaults.params['limit'] = perPage;
-    }
+  setPaginationParams(page = 1, perPage = 20) {
+    this.marvel.defaults.params['offset'] = perPage * (page - 1);
+    this.marvel.defaults.params['limit'] = perPage;
+  }
 }
--- src/js/random_characters.js
+++ src/js/random_characters.js
@@ -1,1 +1,67 @@
 import MarvelAPI from './api_defaults';
+const INTERVAL = 3500;
+const listRef = document.querySelector('.random-characters-list');
+const imgRef = document.querySelector('.img-wrapper');
+
+const marvelApi = new MarvelAPI();
+// marvelApi.setPaginationParams(4, 20);
+const data = await marvelApi.getCharacters();
+console.log(data);
+
+const randomCards = data.sort(() => 0.5 - Math.random());
+let selectedCards = randomCards.slice(0, 5);
+
+let currentCard = selectedCards[0];
+
+const getImgUrl = (obj, size) => {
+  return `${obj.path}/${size}.${obj.extension}`;
+};
+
+const renderImage = () => {
+  imgRef.innerHTML = '';
+  return imgRef.insertAdjacentHTML(
+    'afterbegin',
+    `<img src="${getImgUrl(
+      currentCard.thumbnail,
+      'portrait_uncanny'
+    )}" class="random-characters-img" />`
+  );
+};
+
+function renderItems() {
+  listRef.innerHTML = '';
+  return selectedCards.forEach(item => {
+    listRef.insertAdjacentHTML(
+      'afterbegin',
+      `<li class="random-characters-item">
+          <a href="#" class=" random-item-link ${
+            item.id === currentCard.id ? 'active' : ''
+          }">
+            <h2 class="random-item-title">${item.name}</h2>
+            <p class="random-item-text">${item.description}</p>
+          </a>
+        </li>`
+    );
+  });
+}
+
+let slideIndex = selectedCards.indexOf(currentCard);
+showSlides();
+
+function showSlides() {
+  renderImage();
+  renderItems();
+  let slides = document.getElementsByClassName('random-characters-item');
+  for (let i = 0; i < slides.length; i++) {
+    if (slideIndex === 4) {
+      currentCard = selectedCards[0];
+    } else {
+      currentCard = selectedCards[slideIndex + 1];
+    }
+  }
+  slideIndex++;
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  }
+  setTimeout(showSlides, INTERVAL); // Change image every 3.5 seconds
+}
