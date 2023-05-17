ca4b600794fb8b88597ad847fda8079567d2b227
Favixx <bogdandom52@gmail.com>
2023-05-17 19:06:24 +0300

    Header search setting

--- src/js/header_search.js
+++ src/js/header_search.js
@@ -1,6 +1,7 @@
 const searchHeaderInput = document.querySelector('.search-header')
 const header = document.querySelector('.header')
 const form = document.querySelector('.header_search')
+const buttonSearchHeader = document.querySelector('#header-search')
 window.addEventListener('scroll', scrolBlur)
 function scrolBlur(){
     header.classList.toggle('scroll', window.scrollY > 0)
@@ -8,6 +9,10 @@ function scrolBlur(){
 function submitSearchHandle(event){
     event.preventDefault()
     localStorage.setItem('searchQuery', searchHeaderInput.value);
-    form.submit();
+    form.submit()
 }
 form.addEventListener('submit', submitSearchHandle)
+function submitHeaderForm(){
+    form.submit()
+}
+buttonSearchHeader.addEventListener('click', submitHeaderForm)
