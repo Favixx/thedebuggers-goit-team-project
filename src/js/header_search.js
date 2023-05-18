3b0b371f94adeac22ed3d5a041ff26d64239233a
Favixx <bogdandom52@gmail.com>
2023-05-18 12:57:03 +0300

    Autocomplete done

--- src/js/header_search.js
+++ src/js/header_search.js
@@ -1,83 +1,90 @@
 import MarvelAPI from './api_defaults';
 import { debounce } from 'lodash';
-const searchHeaderInput = document.querySelector('.search-header')
-const header = document.querySelector('.header')
-const form = document.querySelector('.header_search')
-const buttonSearchHeader = document.querySelector('#header-search')
-window.addEventListener('scroll', scrolBlur)
-function scrolBlur(){
-    header.classList.toggle('scroll', window.scrollY > 0)
-}
-function submitSearchHandle(event){
-    event.preventDefault()
-    localStorage.setItem('searchQuery', searchHeaderInput.value);
-    form.submit()
-}
-form.addEventListener('submit', submitSearchHandle)
 
-const apiSearch = new MarvelAPI()
+const searchHeaderInput = document.querySelector('.search-header');
+const header = document.querySelector('.header');
+const form = document.querySelector('.header_search');
+const buttonSearchHeader = document.querySelector('#header-search');
+const autocompleteList = document.querySelector('.autocomplete-list');
 
-async function getDataSearch (inputValue){
-    apiSearch.setPerPage(8)
-    const objectNameInfo = await apiSearch.getNameStartWith(inputValue)
-    const arrName = objectNameInfo.map(el=>{return el.name})
-    
-    return arrName
-    return console.log(arrName);
-}
+const apiSearch = new MarvelAPI();
 
-const searchHeader = document.querySelector('.search-header')
-const autocompleteList = document.querySelector('.autocomplete-list')
-
-searchHeader.addEventListener('input', 
-debounce(
-    async (event)=>{
-        if(event.target.value.trim() === ''){
-            autocompleteList.innerHTML = ''
-            return
-        }
-        const result = await getDataSearch(event.target.value.trim())
-        // console.log(result);
-        if(result !== []){
-            autocompleteList.innerHTML = ''
-            result.forEach(element => {
-                const newElement = element.substring(0, 18) + "..."
-                autocompleteList.insertAdjacentHTML('beforeend', createItemListSearch(newElement))
-            });
-        } else {
-            autocompleteList.innerHTML = ''
-        }
-
-        return result
-    },250)
-    )
-        
-    buttonSearchHeader.addEventListener('click', submitHeaderForm);
-    
-    function submitHeaderForm(){
-        form.submit()
-     }
-function createItemListSearch(newElement){
-   return `
-   <li class="autocomplete-list-item"><a class="autocomplete-list-link" href="">${newElement}</a></li>
-   `
-}
-
-
-// СДЕЛАЛ НО ХУЙ ЕГО НОРМАЛЬНО ЧИ НЕ. НО Я ПРОВЕРИЛ НЕСКОЛЬКО РАЗ, ТО ЧТО ПОПАДАЕТ В ЛОКАЛ ВОПЛНЕ ХВАТАТ ДЛЯ ТОГО ЧТОБЫ ЗАПРОС ПОНЯЛ О ЧЁМ РЕЧь
-
-autocompleteList.addEventListener('click', async (event) => {
-    event.preventDefault();
-    if (event.target.classList.contains('autocomplete-list-link')) {
-        const contentLocal = event.target.textContent.replace('...', '').trim();
-        localStorage.setItem("searchQuery", await getDataSearch(contentLocal));
-        form.submit()
-    }
-
+window.addEventListener('scroll', () => {
+  header.classList.toggle('scroll', window.scrollY > 0);
 });
 
-// Очистить значение поля
-searchHeader.addEventListener('blur', (event)=> {
-    // searchHeader.value = '';
-    autocompleteList.style.display = 'none'
+function submitSearchHandle(event) {
+  event.preventDefault();
+  localStorage.setItem('searchQuery', searchHeaderInput.value);
+  form.submit();
+}
+
+form.addEventListener('submit', submitSearchHandle);
+
+async function getDataSearch(inputValue) {
+  apiSearch.setPerPage(8);
+  const objectNameInfo = await apiSearch.getNameStartWith(inputValue);
+  const arrName = objectNameInfo.map(el => el.name);
+  return arrName;
+}
+
+searchHeaderInput.addEventListener('input', debounce(async (event) => {
+  const inputValue = event.target.value.trim();
+  if (inputValue === '') {
+    autocompleteList.innerHTML = '';
+    return;
+  }
+
+  const result = await getDataSearch(inputValue);
+  autocompleteList.innerHTML = '';
+
+  if (result.length !== 0) {
+    result.forEach(element => {
+        let newElement = element.substring(0, 18) +"..."
+        if(result.length<19){
+        newElement = element
+        }
+      autocompleteList.insertAdjacentHTML('beforeend', createItemListSearch(newElement));
+    });
+  }
+
+  return result;
+}, 250));
+
+buttonSearchHeader.addEventListener('click', () => {
+  form.submit();
+});
+
+function createItemListSearch(newElement) {
+  return `
+    <li class="autocomplete-list-item">${newElement}</li>
+  `;
+}
+
+autocompleteList.addEventListener('click', (event) => {
+    const autocompleteElem = document.querySelectorAll('.autocomplete-list-item');
+    let elementFound = false;
+    let selectedElement = null;
+  
+    autocompleteElem.forEach((elem) => {
+        console.log(elem)
+        console.log(event.target)
+        // console.log(elem===event.currentTarget)
+      if (elem === event.target) {
+        elementFound = true;
+        selectedElement = elem;
+      }
+    });
+  
+    if (elementFound) {
+      const contentLocal = selectedElement.textContent.replace('...', '').trim();
+      localStorage.setItem('searchQueryAutocomplete', contentLocal);
+      form.submit();
+    }
+  });
+// searchHeaderInput.addEventListener('blur', () => {
+//   autocompleteList.style.display = 'none';
+// });
+searchHeaderInput.addEventListener('focus', () => {
+    autocompleteList.style.display = 'block';
   });
